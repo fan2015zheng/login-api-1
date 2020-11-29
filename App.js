5bb610b36f2c4d9b120f48b62c1773e3e722c1f7
Fan Zheng <fan2015zheng@gmail.com>
2020-11-28 18:52:56 -0600

    console.log root url and port

--- App.js
+++ App.js
@@ -6,4 +6,7 @@ app.get('/',(req,res)=>{
   res.send("hello world")
 })
 
-app.listen(config.PORT)
+app.listen(config.PORT, ()=>{
+  console.log( `Root url: ${config.ROOT_URL}`)
+  console.log(`Port: ${config.PORT}`)
+})
